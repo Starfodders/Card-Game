2788c342151483c92cbb412ea367c043289d4f54
Michael Deng <michaeldeng55@gmail.com>
2022-12-25 19:52:57 -0500

    fx for dir

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const webRoot = path.join(process.env.HOME, 'Desktop', 'Card-Game');
+const webRoot = path.join(process.env.HOME, 'Desktop', 'Card-Game');    //states webroot is a location on my personal pc env
 
-app.use(express.static(webRoot));
+app.use(express.static(webRoot));                                       //serves static files from the webroot
+
+const directory = function(file) {                                      //function, avoids writing webroot each time, converts argument into string for location
+    return path.join(webroot, `'${file}'`)
+}
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(webRoot, 'index.html'))
+    res.sendFile(directory(index.html))
 });
 
 app.listen(3000, () => {
